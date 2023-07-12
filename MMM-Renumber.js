756ed9a6ef3c057d21756a1a483bf14690288cdd
JasonVonKrueger <rpoore@gmail.com>
2023-07-12 19:12:59 -0400

    update

--- MMM-Renumber.js
+++ MMM-Renumber.js
@@ -37,13 +37,43 @@ Module.register('MMM-Renumber', {
 
   },
 
+  async handleGuess(data) {
+    document.querySelector('#player_guess').value += data.number;
+    COMPONENT.player_guesses.push(parseInt(data.number));
+
+    if (COMPONENT.player_guesses.length === COMPONENT.numbers.length) {
+      if (COMPONENT.player_guesses.join('') === COMPONENT.numbers.join('')) {
+        COMPONENT.querySelector('#result_message').innerHTML = 'Good job!';
+        await sleep(1200);
+        reset(true);
+      } else {
+        COMPONENT.querySelector('#result_message').innerHTML = 'Nope! Try again.';
+        await sleep(1200);
+        reset(false);
+      }
+    }
+  },
+
   async showNumber() {
     let n = Math.floor(Math.random() * 9)
     document.querySelector('#number').innerHTML = n;
-  
+
     await sleep(1500);
-  
+
     document.querySelector('#number').innerHTML = document.querySelector('#number').innerHTML.replace(/\w|\W/gi, '*');
+  },
+
+  reset(result) {
+    if (result) COMPONENT.level++
+    else COMPONENT.level = 1
+
+    COMPONENT.numbers.length = 0;
+    COMPONENT.player_guesses.length = 0;
+    COMPONENT.querySelector('#player_message').classList.add('hidden');
+    COMPONENT.querySelector('#player_guess').value = '';
+    COMPONENT.querySelector('#result_message').innerHTML = '';
+
+    showNumber();
   },
 
   sleep(ms) {
