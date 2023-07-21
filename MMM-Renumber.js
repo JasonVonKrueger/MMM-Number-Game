7a4bf84d13a45da2a4ab5e71de1a632ce11b8af1
JasonVonKrueger <rpoore@gmail.com>
2023-07-20 20:43:49 -0400

    updates

--- MMM-Renumber.js
+++ MMM-Renumber.js
@@ -5,10 +5,11 @@ Module.register('MMM-Renumber', {
 
   start() {
     //const self = this;
+    this.hide();
     this.numbers = [];
     this.player_guesses = [];
     this.level = 1;
-    //this.hidden = true;
+    this.hidden = true;
     this.max_levels = this.config.maxLevels || 10;
 
     //Log.info("Starting module: " + this.name);
@@ -16,14 +17,14 @@ Module.register('MMM-Renumber', {
   },
 
   getStyles() {
-    return ["MMM-Renumber.css"];
+    return ['MMM-Renumber.css'];
   },
 
   getDom() {
     const wrapper = document.createElement('div');
 
     let markup = `
-          <div id="mmm-renumber" class="hidden">
+          <div id="mmm-renumber">
             <div class="title">Renumber</div>
             <div id="game_box">
             <div id="number"></div>
@@ -44,51 +45,51 @@ Module.register('MMM-Renumber', {
 
   },
 
-  // async handleGuess(data) {
-  //   document.querySelector('#player_guess').value += data.number;
-  //   this.player_guesses.push(parseInt(data.number));
+  async handleGuess(data) {
+    document.querySelector('#player_guess').value += data.number;
+    this.player_guesses.push(parseInt(data.number));
 
-  //   if (this.player_guesses.length === this.numbers.length) {
-  //     if (this.player_guesses.join('') === this.numbers.join('')) {
-  //       this.querySelector('#result_message').innerHTML = 'Good job!';
-  //       await this.sleep(1200);
-  //       reset(true);
-  //     } 
-  //     else {
-  //       this.querySelector('#result_message').innerHTML = 'Nope! Try again.';
-  //       await this.sleep(1200);
-  //       reset(false);
-  //     }
-  //   }
-  // },
+    if (this.player_guesses.length === this.numbers.length) {
+      if (this.player_guesses.join('') === this.numbers.join('')) {
+        this.querySelector('#result_message').innerHTML = 'Good job!';
+        await this.sleep(1200);
+        reset(true);
+      } 
+      else {
+        this.querySelector('#result_message').innerHTML = 'Nope! Try again.';
+        await this.sleep(1200);
+        reset(false);
+      }
+    }
+  },
 
-  // async showNumber() {
-  //   let n = Math.floor(Math.random() * 9)
-  //   document.querySelector('#number').innerHTML = n;
+  async showNumber() {
+    let n = Math.floor(Math.random() * 9)
+    document.querySelector('#number').innerHTML = n;
 
-  //   await this.sleep(1500);
+    await this.sleep(1500);
 
-  //   document.querySelector('#number').innerHTML = document.querySelector('#number').innerHTML.replace(/\w|\W/gi, '*');
-  // },
+    document.querySelector('#number').innerHTML = document.querySelector('#number').innerHTML.replace(/\w|\W/gi, '*');
+  },
 
-  // reset(result) {
-  //   if (result) this.level++;
-  //   else this.level = 1;
+  reset(result) {
+    if (result) this.level++;
+    else this.level = 1;
 
-  //   //this.config.maxLevels
+    //this.config.maxLevels
 
-  //   this.numbers.length = 0;
-  //   this.player_guesses.length = 0;
-  //   this.querySelector('#player_message').classList.add('hidden');
-  //   this.querySelector('#player_guess').value = '';
-  //   this.querySelector('#result_message').innerHTML = '';
+    this.numbers.length = 0;
+    this.player_guesses.length = 0;
+    this.querySelector('#player_message').classList.add('hidden');
+    this.querySelector('#player_guess').value = '';
+    this.querySelector('#result_message').innerHTML = '';
 
-  //   //this.showNumber();
-  // },
+    //this.showNumber();
+  },
 
-  // sleep(ms) {
-  //   return new Promise(resolve => setTimeout(resolve, ms));
-  // },
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  },
 
   // sendSocketNotification(notification, payload) {
   //   this.socket().sendNotification(notification, payload);
@@ -106,7 +107,7 @@ Module.register('MMM-Renumber', {
 
   socketNotificationReceived(notification, payload) {
     if (notification === 'RENUMBER_CLIENT_CONNECTED') {
-      document.querySelector('#mmm-renumber').classList.remove('hidden');
+      //document.querySelector('#mmm-renumber').classList.remove('hidden');
     }
 
     
