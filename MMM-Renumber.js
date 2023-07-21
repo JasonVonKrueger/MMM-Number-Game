efbc8d5bc50a1b546e9cbbe9ab60c1c5b2e78745
JasonVonKrueger <rpoore@gmail.com>
2023-07-20 20:18:26 -0400

    updates

--- MMM-Renumber.js
+++ MMM-Renumber.js
@@ -95,7 +95,8 @@ Module.register('MMM-Renumber', {
   // },
 
   notificationReceived(notification, payload, sender) {
-    //this.sendSocketNotification("blah_blah");
+    this.sendSocketNotification("blah_blah");
+    
     if (sender) {
       Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
     } else {
